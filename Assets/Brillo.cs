1726bd43bfc64955ef2cd6cb4c677c6831a23a0e
Fabrizio Valero <fabrizio1502ips@gmail.com>
2024-10-02 15:23:09 -0300

    Integrar menu

---
+++ Assets/Brillo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Brillo : MonoBehaviour
+{
+    public Slider slider;
+    public float valorSlider;
+    public Image panelBrillo;
+
+    void Start()
+    {
+        // Establece el valor inicial del slider desde PlayerPrefs
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.4f);
+        ActualizarBrillo(slider.value);  // Aplica el valor del slider al brillo inicial
+    }
+
+    // Método que se llama cuando el valor del slider cambia
+    public void ChangeBrillo(float valor)
+    {
+        valorSlider = valor;
+        // Guarda el valor del brillo en PlayerPrefs
+        PlayerPrefs.SetFloat("brillo", valorSlider);
+        // Actualiza el color del panelBrillo con el valor del slider
+        ActualizarBrillo(valorSlider);
+    }
+
+    // Método auxiliar para actualizar el brillo
+    void ActualizarBrillo(float valor)
+    {
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, valor);
+    }
+}
